70adb66274c74331fc90bab8832d9976095adacc
darrelhong <darrel.hong@gmail.com>
2023-10-07 16:20:23 +0800

    feat: add dynamic fomr for puzzles form

--- src/app/components/PuzzleForm.tsx
+++ src/app/components/PuzzleForm.tsx
@@ -1,36 +1,82 @@
 'use client'
 
 import { useAtom } from 'jotai'
+import { useEffect, useState } from 'react'
 import { useFieldArray, useForm } from 'react-hook-form'
 
 import { puzzlesAtom } from '@/utils/puzzles'
 
 export function PuzzleForm() {
-  const [puzzles] = useAtom(puzzlesAtom)
+  const [mounted, setHasMounted] = useState(false)
 
-  const { register, control } = useForm({
-    defaultValues: {
-      puzzles: puzzles.map((puzzleStr) => ({ puzzleStr })),
-    },
+  const [puzzles, setPuzzlesAtom] = useAtom(puzzlesAtom)
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<{
+    puzzles: {
+      puzzleStr: string
+    }[]
+  }>({
+    mode: 'onChange',
   })
-  const { fields, remove, append } = useFieldArray({
+
+  const { fields, remove, append, replace } = useFieldArray({
     control,
     name: 'puzzles',
   })
 
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  useEffect(() => {
+    replace(puzzles.map((puzzleStr) => ({ puzzleStr })))
+  }, [puzzles, replace])
+
+  if (!mounted) {
+    return null
+  }
+
   return (
-    <form className="w-full max-w-xl">
+    <form
+      className="w-full max-w-xl"
+      onSubmit={handleSubmit((data) => {
+        setPuzzlesAtom(data.puzzles.map(({ puzzleStr }) => puzzleStr))
+      })}
+    >
       <ul className="grid gap-2">
-        {fields.map((item, index) => (
-          <li key={item.id} className="grid gap-0.5">
+        {fields.map((field, index) => (
+          <li key={field.id} className="grid gap-0.5">
             <label htmlFor={`puzzles.${index}.puzzleStr`}>
               Puzzle {index + 1}
             </label>
-            <textarea
+            <input
               className="input"
-              {...register(`puzzles.${index}.puzzleStr`)}
+              {...register(`puzzles.${index}.puzzleStr`, {
+                validate: (value) => {
+                  if (value.length !== 81) {
+                    return 'Puzzle must be 81 characters long'
+                  }
+                  if (value.match(/[^1-9.]/)) {
+                    return 'Puzzle must only contain numbers 1-9 and .(period)'
+                  }
+                  return true
+                },
+              })}
               id={`puzzles.${index}.puzzleStr`}
+              aria-invalid={
+                errors.puzzles?.[index]?.puzzleStr ? 'true' : 'false'
+              }
             />
+            {errors.puzzles?.[index]?.puzzleStr && (
+              <p className="text-sm text-red-400">
+                {errors.puzzles?.[index]?.puzzleStr?.message}
+              </p>
+            )}
             <button
               className="btn justify-self-start text-sm"
               type="button"
--- src/utils/puzzles.ts
+++ src/utils/puzzles.ts
@@ -1,6 +1,6 @@
-import { atom } from 'jotai'
+import { atomWithStorage } from 'jotai/utils'
 
-export const puzzlesAtom = atom([
+export const puzzlesAtom = atomWithStorage('puzzles', [
   '52...6.........7.13...........4..8..6......5...........418.........3..2...87.....',
   '837629145.4.318..2921574368.54186239163...8.7289.53416..28.56.1...241..3318967524',
   '634.28.15...456283528.13.6.45.397128213865.4.8..14.5.6.6.58..91381279654945631872',
