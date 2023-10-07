50fae0f0d690c16abfe28d828b00b31b31157eec
darrelhong <darrel.hong@gmail.com>
2023-10-07 15:10:47 +0800

    feat: add react-hook-form and setup dynamic fields

--- src/app/components/PuzzleForm.tsx
+++ src/app/components/PuzzleForm.tsx
@@ -1,24 +1,61 @@
 'use client'
 
 import { useAtom } from 'jotai'
+import { useFieldArray, useForm } from 'react-hook-form'
 
 import { puzzlesAtom } from '@/utils/puzzles'
 
 export function PuzzleForm() {
   const [puzzles] = useAtom(puzzlesAtom)
 
+  const { register, control } = useForm({
+    defaultValues: {
+      puzzles: puzzles.map((puzzleStr) => ({ puzzleStr })),
+    },
+  })
+  const { fields, remove, append } = useFieldArray({
+    control,
+    name: 'puzzles',
+  })
+
   return (
-    <form className="grid w-full max-w-xl gap-2">
-      {puzzles.map((puzzleStr, index) => (
-        <div key={index} className="grid gap-0.5">
-          <label htmlFor={`puzzle-${index}`}>Puzzle {index + 1}</label>
-          <textarea
-            id={`puzzle-${index}`}
-            defaultValue={puzzleStr}
-            className="input"
-          />
-        </div>
-      ))}
+    <form className="w-full max-w-xl">
+      <ul className="grid gap-2">
+        {fields.map((item, index) => (
+          <li key={item.id} className="grid gap-0.5">
+            <label htmlFor={`puzzles.${index}.puzzleStr`}>
+              Puzzle {index + 1}
+            </label>
+            <textarea
+              className="input"
+              {...register(`puzzles.${index}.puzzleStr`)}
+              id={`puzzles.${index}.puzzleStr`}
+            />
+            <button
+              className="btn justify-self-start text-sm"
+              type="button"
+              onClick={() => remove(index)}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <section className="mt-3">
+        <button
+          className="btn text-sm"
+          type="button"
+          onClick={() => append({ puzzleStr: '' })}
+        >
+          Add
+        </button>
+      </section>
+      <section className="mt-3">
+        <button className="btn" type="submit">
+          Save
+        </button>
+      </section>
     </form>
   )
 }
